ec0763363c4c22ef2a5c7f0b51ff4152fcdaeaa8
Kirill Sayed <sayed.kirill@gmail.com>
2020-11-15 11:39:35 +0300

    renamed main.py to secret_key_generator.py

---
+++ secret_key_generator.py
@@ -0,0 +1,11 @@
+"""secret_key_generator.py file"""
+from generator_class import SecretKeyGenerator
+
+
+def generate() -> str:
+    """
+    Returns:
+        generator.generate(): secret key (str)
+    """
+    generator = SecretKeyGenerator()
+    return generator.generate()
