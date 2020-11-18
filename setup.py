7bb68b0e708c4a13182e93be6ab52ad636361413
Kirill Sayed <sayed.kirill@gmail.com>
2020-11-18 22:13:23 +0300

    Updated version to 0.0.7

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os
-
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -7,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="secret-key-generator",
-    version=os.environ.get("RELEASE_NAME"),
+    version="0.0.7",
     author="Kirill Sayed",
     author_email="sayed.kirill@gmail.com",
     description="Package for secret key generation in Django framework.",
